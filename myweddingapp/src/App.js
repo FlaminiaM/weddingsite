475b860d79a0f171d56310dd6aff8fbf149b167e
Flaminia Mazzitelli <flaminia.mazzitelli.92@gmail.com>
2022-09-04 19:05:43 +0100

    added some components

--- myweddingapp/src/App.js
+++ myweddingapp/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.scss';
+import Button from './components/atoms/button/Button';
+import Icon from './components/atoms/icon/Icon';
+import Line from './components/atoms/line/Line';
+import MenuItem from './components/atoms/menuItem/MenuItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="button" text="RSVP" />
+      <h1>Hello World</h1>
+      <Line type="divider" orientation="horizontal" />
+      <Line type="timeline" orientation="horizontal" />
+      <Line type="timeline" orientation="vertical" />
+      <Icon name="church" size="small"/>
+      <Icon name="church" size="medium"/>
+      <Icon name="church" size="large"/>
+      <MenuItem url="#" text="Example"/>
     </div>
   );
 }
