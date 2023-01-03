8d0eaf73fe125e152b4942048e801ece7c057ead
Mazzitelli, Flaminia <flaminia.mazzitelli@virginmediao2.co.uk>
2023-01-03 08:44:01 +0000

    test

--- myweddingapp/src/components/organisms/sideDrawer/SideDrawer.js
+++ myweddingapp/src/components/organisms/sideDrawer/SideDrawer.js
@@ -8,38 +8,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SideDrawer({isOpen, handleDrawerState, drawerContent}) {
   let elementClasses;
-  function disableScroll() {
-    // Get the current page scroll position in the vertical direction
-   let scrollTop =
-       window.pageYOffset || document.documentElement.scrollTop;
-        alert(scrollTop)
-        
-// Get the current page scroll position in the horizontal direction 
-
- let scrollLeft =
-   window.pageXOffset || document.documentElement.scrollLeft;
-   
-   alert(scrollLeft)
-  // if any scroll is attempted,
- // set this to the previous value
- window.onscroll = function() {
-  window.scrollTo(scrollLeft, scrollTop);
- };
-}
-
-  function enableScroll() {
-     window.onscroll = function() {};
- }
   if(isOpen === null){
     elementClasses = "side-drawer";
   } else if(!isOpen){
     document.querySelector("body").classList.remove("no-scroll");
     elementClasses = "side-drawer slide-out-right";
-    enableScroll();
+    
   } else if(isOpen){
     document.querySelector("body").classList.add("no-scroll");
     elementClasses = "side-drawer slide-in-right";
-    disableScroll();
+    alert(window.screen.height)
   }
   return (
     <>
