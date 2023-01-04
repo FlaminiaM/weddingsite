1145eae8c21410f34c03b12d6cefd4c173ee48eb
Mazzitelli, Flaminia <flaminia.mazzitelli@virginmediao2.co.uk>
2023-01-04 17:19:30 +0000

    fixed menu to update on scroll

--- myweddingapp/src/components/atoms/menuItem/MenuItem.js
+++ myweddingapp/src/components/atoms/menuItem/MenuItem.js
@@ -3,14 +3,15 @@ import './MenuItem.scss';
 
 import { Link } from "react-scroll";
 
-function MenuItem({text, url}) {
+function MenuItem({text, url, menuButtonHandler}) {
   const handleSelection = (e) => {
     document.querySelectorAll(".menu-item-link").forEach(el => el.classList.remove("menu-item-link--active"))
-    e.target.classList.add("menu-item-link--active")
+    e.target.classList.add("menu-item-link--active");
+    menuButtonHandler();
   }
   return (
     <li className='menu-item'>
-        <Link className={url === "page-homepage" ? 'menu-item-link menu-item-link--active' :'menu-item-link'} to={url} smooth={true} onClick={(e) => handleSelection(e)}>{text}</Link>
+        <Link className='menu-item-link' activeClass="menu-item-link--active" to={url} smooth={true} spy={true} onClick={(e) => handleSelection(e)}>{text}</Link>
     </li>
   )
 }
